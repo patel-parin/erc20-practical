4c3314f83953e25209cb977c8f5e25199d97ad50
GH1234-X <patelppr5@gmail.com>
2026-02-06 13:46:03 +0530

    complete Transferring Tokens and Approving Token Transfers

--- contracts/parin.sol
+++ contracts/parin.sol
@@ -4,10 +4,7 @@ contract parin {
     string  public name = "parin";
     string  public symbol = "parin";
     string  public standard = "parin v1.0";
-
     uint256 public totalSupply;
-
-    mapping(address => uint256) public balanceOf;
 
     event Transfer(
         address indexed _from,
@@ -15,15 +12,22 @@ contract parin {
         uint256 _value
     );
 
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
     function parin(uint256 _initialSupply) public {
         totalSupply = _initialSupply;
         balanceOf[msg.sender] = _initialSupply;
     }
 
-    function transfer(address _to, uint256 _value) public returns (bool success) {
-    if (balanceOf[msg.sender] < _value) {
-        return false;
-    }
+ function transfer(address _to, uint256 _value) public returns (bool success) {
+    require(balanceOf[msg.sender] >= _value);
 
     balanceOf[msg.sender] -= _value;
     balanceOf[_to] += _value;
@@ -31,5 +35,13 @@ contract parin {
     Transfer(msg.sender, _to, _value);
     return true;
 }
+
+function approve(address _spender, uint256 _value) public returns (bool success) {
+    allowance[msg.sender][_spender] = _value;
+
+    Approval(msg.sender, _spender, _value);
+    return true;
+}
+
 
 }
--- test/parin.js
+++ test/parin.js
@@ -16,7 +16,7 @@ contract('parin', function(accounts) {
     }).then(function(standard) {
       assert.equal(standard, 'parin v1.0', 'has the correct standard');
     });
-  })
+  });
 
   it('allocates the initial supply upon deployment', function() {
     return parin.deployed().then(function(instance) {
@@ -31,35 +31,39 @@ contract('parin', function(accounts) {
   });
 
   it('transfers token ownership', function() {
-  return parin.deployed().then(function(instance) {
-    tokenInstance = instance;
-
-    // This should FAIL and return false (not revert)
-return tokenInstance.transfer.call(accounts[1], 2000000);
-  }).then(function(success) {
-    assert.equal(success, false, 'transfer should fail when balance is low');
-
-    // Valid transfer check
-    return tokenInstance.transfer.call(accounts[1], 250000, { from: accounts[0] });
-  }).then(function(success) {
-    assert.equal(success, true, 'it returns true');
-
-    return tokenInstance.transfer(accounts[1], 250000, { from: accounts[0] });
-  }).then(function(receipt) {
-    assert.equal(receipt.logs.length, 1, 'triggers one event');
-    assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
-    assert.equal(receipt.logs[0].args._from, accounts[0]);
-    assert.equal(receipt.logs[0].args._to, accounts[1]);
-    assert.equal(receipt.logs[0].args._value.toNumber(), 250000);
-
-    return tokenInstance.balanceOf(accounts[1]);
-  }).then(function(balance) {
-    assert.equal(balance.toNumber(), 250000);
-
-    return tokenInstance.balanceOf(accounts[0]);
-  }).then(function(balance) {
-    assert.equal(balance.toNumber(), 750000);
+    return parin.deployed().then(function(instance) {
+      tokenInstance = instance;
+      return tokenInstance.transfer.call(accounts[1], 25000000);
+    }).then(assert.fail).catch(function(error) {
+      assert(error.message.indexOf('revert') >= 0, 'error message must contain revert');
+      return tokenInstance.transfer.call(accounts[1], 250000, { from: accounts[0] });
+    }).then(function(success) {
+      assert.equal(success, true, 'it returns true');
+      return tokenInstance.transfer(accounts[1], 250000, { from: accounts[0] });
+    }).then(function(receipt) {
+      assert.equal(receipt.logs.length, 1, 'triggers one event');
+      assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
+      assert.equal(receipt.logs[0].args._from, accounts[0]);
+      assert.equal(receipt.logs[0].args._to, accounts[1]);
+      assert.equal(receipt.logs[0].args._value, 250000);
+    });
   });
-});
 
+  it('approves tokens for delegated transfer', function() {
+    return parin.deployed().then(function(instance) {
+      tokenInstance = instance;
+      return tokenInstance.approve.call(accounts[1], 100);
+    }).then(function(success) {
+      assert.equal(success, true);
+      return tokenInstance.approve(accounts[1], 100, { from: accounts[0] });
+    }).then(function(receipt) {
+      assert.equal(receipt.logs.length, 1);
+      assert.equal(receipt.logs[0].event, 'Approval');
+      return tokenInstance.allowance(accounts[0], accounts[1]);
+    }).then(function(allowance) {
+      assert.equal(allowance.toNumber(), 100);
+    });
+  });
+
+ 
 });
